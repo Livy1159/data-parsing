982c86af0fc9310437f1f1d33a399766e7f212ef
Olivia Morgan <morgan.olivia@gmail.com>
2018-01-17 12:14:23 -0500

    Format rows from csv files

--- parse.rb
+++ parse.rb
@@ -1,7 +1,25 @@
 require 'csv'
 
-csv = CSV.read('file2.csv', headers: false)
+csv_array = []
 
-csv.each do |row|
-  p row
+csv1 = CSV.read('file1.csv', headers: false)
+
+csv1.each do |row|
+  split_array1 = row[0].split(' ')
+  csv_array << split_array1
 end
+
+csv2 = CSV.read('file2.csv', headers: false)
+
+csv2.each do |row|
+  split_array2 = row[0].split('|')
+  csv_array << split_array2
+end
+
+csv3 = CSV.read('file3.csv', headers: false)
+
+csv3.each do |row|
+  csv_array << row
+end
+
+p csv_array
